22170cb7517aa1df853e1c60bee0a0d887d5e5e7
大崎    一史 <kzfmmacpc@KZFMMacPC.local>
2025-04-03 00:14:20 +0900

    アセットパイプラインの設定を修正：サブディレクトリのパスを追加し、CSSファイルのプリコンパイル設定を追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,23 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'base')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'components')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'layouts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'pages')
+
+# Precompile additional assets.
+Rails.application.config.assets.precompile += %w( *.css )
+
+# Enable serving of static files from the `/public` folder by default
+Rails.application.config.serve_static_files = true
+
+# Enable serving of static files from the `/public` folder in development
+Rails.application.config.serve_static_assets = true
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
