f660a66543e113c3376f3fb0edd5a03cc762e8a3
大崎    一史 <kzfmmacpc@KZFMMacPC.local>
2025-04-17 21:44:55 +0900

    style: Rubocopの自動修正を適用 - 文字列リテラルをダブルクォートに統一、配列リテラル内のスペースを修正、末尾の空白を削除

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,4 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :bio, :avatar)
   end
-end 
+end
