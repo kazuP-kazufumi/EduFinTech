20d2d84be9d741700141e6316904d629ef358743
大崎    一史 <kzfmmacpc@KZFMMacPC.local>
2025-04-17 10:51:59 +0900

    pendingマークの適用方法を修正: RSpecのテスト構文エラーを解消

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,88 +1,84 @@
 # MessagesControllerのテスト
 RSpec.describe MessagesController, type: :controller do
   # 認証・認可のテスト
-  describe '認証・認可' do
-    pending 'MessagesControllerの認証テストを実装する' do
-      context '未ログインユーザーの場合' do
-        let(:chat_room) { create(:chat_room) }
+  describe '認証・認可', skip: 'MessagesControllerの認証テストを実装する' do
+    context '未ログインユーザーの場合' do
+      let(:chat_room) { create(:chat_room) }
+
+      it 'メッセージ一覧にアクセスできないこと' do
+        get :index, params: { chat_room_id: chat_room.id }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'メッセージを作成できないこと' do
+        post :create, params: { chat_room_id: chat_room.id, message: attributes_for(:message) }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'ログインユーザーの場合' do
+      before do
+        sign_in_test_user
+        @chat_room = create(:chat_room)
+      end
+
+      it 'メッセージ一覧にアクセスできること' do
+        get :index, params: { chat_room_id: @chat_room.id }
+        expect(response).to be_successful
+      end
+
+      it 'メッセージを作成できること' do
+        expect {
+          post :create, params: { chat_room_id: @chat_room.id, message: attributes_for(:message) }
+        }.to change(Message, :count).by(1)
+      end
+
+      context 'チャットルームのメンバーでない場合' do
+        let(:other_chat_room) { create(:chat_room) }
 
         it 'メッセージ一覧にアクセスできないこと' do
-          get :index, params: { chat_room_id: chat_room.id }
-          expect(response).to redirect_to(new_user_session_path)
+          get :index, params: { chat_room_id: other_chat_room.id }
+          expect(response).to redirect_to(root_path)
         end
 
         it 'メッセージを作成できないこと' do
-          post :create, params: { chat_room_id: chat_room.id, message: attributes_for(:message) }
-          expect(response).to redirect_to(new_user_session_path)
-        end
-      end
-
-      context 'ログインユーザーの場合' do
-        before do
-          sign_in_test_user
-          @chat_room = create(:chat_room)
-        end
-
-        it 'メッセージ一覧にアクセスできること' do
-          get :index, params: { chat_room_id: @chat_room.id }
-          expect(response).to be_successful
-        end
-
-        it 'メッセージを作成できること' do
-          expect {
-            post :create, params: { chat_room_id: @chat_room.id, message: attributes_for(:message) }
-          }.to change(Message, :count).by(1)
-        end
-
-        context 'チャットルームのメンバーでない場合' do
-          let(:other_chat_room) { create(:chat_room) }
-
-          it 'メッセージ一覧にアクセスできないこと' do
-            get :index, params: { chat_room_id: other_chat_room.id }
-            expect(response).to redirect_to(root_path)
-          end
-
-          it 'メッセージを作成できないこと' do
-            post :create, params: { chat_room_id: other_chat_room.id, message: attributes_for(:message) }
-            expect(response).to redirect_to(root_path)
-          end
+          post :create, params: { chat_room_id: other_chat_room.id, message: attributes_for(:message) }
+          expect(response).to redirect_to(root_path)
         end
       end
     end
   end
 
   # アクションのテスト
-  describe 'POST #create' do
-    pending 'MessagesControllerのcreateアクションテストを実装する' do
-      before do
-        sign_in_test_user
-        @chat_room = create(:chat_room)
-      end
+  describe 'POST #create', skip: 'MessagesControllerのcreateアクションテストを実装する' do
+    before do
+      sign_in_test_user
+      @chat_room = create(:chat_room)
+    end
 
-      context '有効なパラメータの場合' do
-        it 'メッセージを作成すること' do
-          expect {
-            post :create, params: { chat_room_id: @chat_room.id, message: attributes_for(:message) }
-          }.to change(Message, :count).by(1)
-        end
-
-        it 'チャットルームページにリダイレクトすること' do
+    context '有効なパラメータの場合' do
+      it 'メッセージを作成すること' do
+        expect {
           post :create, params: { chat_room_id: @chat_room.id, message: attributes_for(:message) }
-          expect(response).to redirect_to(@chat_room)
-        end
+        }.to change(Message, :count).by(1)
       end
 
-      context '無効なパラメータの場合' do
-        it 'メッセージを作成しないこと' do
-          expect {
-            post :create, params: { chat_room_id: @chat_room.id, message: attributes_for(:message, content: nil) }
-          }.not_to change(Message, :count)
-        end
+      it 'チャットルームページにリダイレクトすること' do
+        post :create, params: { chat_room_id: @chat_room.id, message: attributes_for(:message) }
+        expect(response).to redirect_to(@chat_room)
+      end
+    end
 
-        it 'チャットルームページにリダイレクトすること' do
+    context '無効なパラメータの場合' do
+      it 'メッセージを作成しないこと' do
+        expect {
           post :create, params: { chat_room_id: @chat_room.id, message: attributes_for(:message, content: nil) }
-          expect(response).to redirect_to(@chat_room)
-        end
+        }.not_to change(Message, :count)
+      end
+
+      it 'チャットルームページにリダイレクトすること' do
+        post :create, params: { chat_room_id: @chat_room.id, message: attributes_for(:message, content: nil) }
+        expect(response).to redirect_to(@chat_room)
       end
     end
   end
